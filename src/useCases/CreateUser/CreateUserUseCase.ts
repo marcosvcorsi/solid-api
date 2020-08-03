07617880ae9f868b306e87e3bdb56c93fa38c7b8
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-02 21:37:59 -0300

    Request adicionada

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,9 +1,13 @@
 import IUsersRepository from '../../repositories/IUsersReposity';
 import ICreateUserDTO from './CreateUserDTO';
 import User from '../../entities/User';
+import IMailProvider from '../../providers/IMailProvider';
 
 export default class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
 
   async execute({ email, name, password }: ICreateUserDTO) {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
@@ -15,5 +19,18 @@ export default class CreateUserUseCase {
     const user = new User({ email, name, password });
 
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        email,
+        name,
+      },
+      from: {
+        email: 'equipe@app.com.br',
+        name: 'Equipe',
+      },
+      subject: 'Cadastro de Usuário',
+      body: 'Seja bem vindo',
+    });
   }
 }
